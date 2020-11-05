6c6968f74a0cad435de77c57d5778ba5a0b97c52
QamarJaradat <moon19.qs@gmail.com>
2020-11-05 11:21:38 +0200

    By works, I meant 'doesnt work'. Works now.. Stil NOT WORKING

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from "react-dom";
+// import React from 'react'
+// import ReactDOM from "react-dom";
 // import ReactDev from 'react-dev'
 // class App extends fro
 

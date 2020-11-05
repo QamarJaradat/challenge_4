f63982f962ce6ca5de7ae72242f42787da3b7f28
QamarJaradat <moon19.qs@gmail.com>
2020-11-05 15:22:37 +0200

    WTH is thisgit add .

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,39 @@
-// import React from 'react'
-// import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from "react-dom";
 // import ReactDev from 'react-dev'
-// class App extends fro
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.choosesquer = this.choosesquer.bind(this)
+    }
+    choosesquer() {
+        console.log('clicked')
+    }
+    render() {
+        return (<div onClick={this.choosesquer}>react</div>)
+    }
+}
+
+class Bord extends React.Component {
+
+}
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return (
+            <div></div>
+        )
+    }
+
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
 
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var server = express()
 
-server.use(express.static(__dirname + '/client'));
+server.use(express.static('client/dist'));
 
 
-server.listen(3000)
-console.log("Server Listening on 3000")
+server.listen(3000, () => {
+    console.log("Server Listening on 3000")
+})
+
 
